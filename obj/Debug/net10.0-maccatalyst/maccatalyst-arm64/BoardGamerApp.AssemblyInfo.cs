c083b686b71a953ede012a81aa6dc95c3f537b19
Sebastian Stark <sstark@gmx.net>
2026-06-06 11:15:28 +0200

    Terminverwaltung: Anpassungen UI EventPage.xaml

--- obj/Debug/net10.0-maccatalyst/maccatalyst-arm64/BoardGamerApp.AssemblyInfo.cs
+++ obj/Debug/net10.0-maccatalyst/maccatalyst-arm64/BoardGamerApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BoardGamerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+63d4393704f24ff59c47286d15603dd85f469ba1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+8f97b4be462ba46f0d986617cd170ee8468cb6d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BoardGamerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BoardGamerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
